9416f6f5fa7e2475b1565222439161e223ea2ba9
John Reese <john@noswap.com>
2012-04-28 18:42:02 -0700

    Distutils definition

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(name='Pyranha',
+      description='Elegant IRC client',
+      version='0.1',
+      author='John Reese',
+      author_email='john@noswap.com',
+      url='https://github.com/jreese/pyranha',
+      classifiers=['License :: OSI Approved :: MIT License',
+                   'Topic :: Communications :: Chat :: Internet Relay Chat',
+                   'Development Status :: 2 - Pre-Alpha',
+                   ],
+      license='MIT License',
+      data_files=[('/usr/local/bin', ['bin/pyranha'])],
+      packages=['pyranha', 'pyranha.irc'],
+      )
