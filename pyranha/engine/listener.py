2ee01234ab6d2f855270c4a2c42587f296d93bf1
John Reese <john@noswap.com>
2012-08-05 21:44:06 -0700

    Add listener support for directly dispatching on IRC events

--- pyranha/engine/listener.py
+++ pyranha/engine/listener.py
@@ -4,12 +4,24 @@
 from __future__ import absolute_import, division
 
 from pyranha.logging import log
+import pyranha.irc.client
 
 class Listener(object):
 
-    def notify(self, event_type, *args, **params):
+    def notify(self, *args, **params):
         try:
+            event = args[0]
+
+            t = type(event)
+            if t == str:
+                event_type = event
+                args.pop(0)
+            elif t == pyranha.irc.client.Event:
+                event_type = event.eventtype()
+
+            event_type = event_type.replace('-', '_')
             method = getattr(self, 'on_' + event_type)
+
         except AttributeError:
             log.warning('{0} does not support {1}'.format(self, event_type))
         else:
